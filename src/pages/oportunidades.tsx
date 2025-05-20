7323cae4bec469e71bd18f9184a960e451230453
Thiago Rotondo <Rotondo@users.noreply.github.com>
2025-05-20 11:32:07 -0300

    Create oportunidades.tsx

---
+++ src/pages/oportunidades.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const OportunidadesPage = () => (
+  <main style={{ padding: 24 }}>
+    <h1>Oportunidades</h1>
+    <p>Página de oportunidades! Implemente aqui sua listagem ou funcionalidades.</p>
+  </main>
+);
+
+export default OportunidadesPage;
