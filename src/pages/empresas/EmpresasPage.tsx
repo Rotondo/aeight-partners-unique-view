8ae898eb4704bfa0e0d388c2f07abc31ec046be3
Thiago Rotondo <Rotondo@users.noreply.github.com>
2025-05-21 15:15:48 -0300

    Create EmpresasPage.tsx

---
+++ src/pages/empresas/EmpresasPage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const EmpresasPage: React.FC = () => {
+  return (
+    <div>
+      <h1 className="text-2xl font-bold mb-4">Empresas</h1>
+      <p>Página de Empresas em construção.</p>
+    </div>
+  );
+};
+
+export default EmpresasPage;
