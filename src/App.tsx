cc059465fdf22e543550920b57055393a65d3c7c
Thiago Rotondo <Rotondo@users.noreply.github.com>
2025-05-21 16:09:40 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Outlet } from "react-router-dom";
 import MainLayout from "@/components/layout/MainLayout";
 import DashboardPage from "@/pages/dashboard/DashboardPage";
+import OportunidadesPage from "@/pages/oportunidades/OportunidadesPage";
+import IndicadoresPage from "@/pages/indicadores/IndicadoresPage";
+import EmpresasPage from "@/pages/empresas/EmpresasPage";
 import NotFoundPage from "@/pages/NotFoundPage";
 import LoginPage from "@/pages/LoginPage";
 import LoadingScreen from "@/components/ui/LoadingScreen";
 import { AuthProvider } from "@/hooks/useAuth";
 import PrivateRoute from "@/components/PrivateRoute";
+import OnePagerPage from "@/pages/onepager/OnePagerPage";
+import QuadrantePage from "@/pages/quadrante/QuadrantePage";
+import Admin from "@/pages/admin";
+import { Toaster } from "@/components/ui/toaster";
 
 const App: React.FC = () => (
   <Router>
@@ -24,11 +31,16 @@ const App: React.FC = () => (
           >
             {/* Rotas internas protegidas */}
             <Route index element={<DashboardPage />} />
-            {/* Adicione aqui outras rotas internas, exemplo: */}
-            {/* <Route path="outra-pagina" element={<OutraPagina />} /> */}
+            <Route path="oportunidades" element={<OportunidadesPage />} />
+            <Route path="indicadores" element={<IndicadoresPage />} />
+            <Route path="empresas" element={<EmpresasPage />} />
+            <Route path="onepager/*" element={<OnePagerPage />} />
+            <Route path="quadrante" element={<QuadrantePage />} />
+            <Route path="admin" element={<Admin />} />
             <Route path="*" element={<NotFoundPage />} />
           </Route>
         </Routes>
+        <Toaster />
       </React.Suspense>
     </AuthProvider>
   </Router>
