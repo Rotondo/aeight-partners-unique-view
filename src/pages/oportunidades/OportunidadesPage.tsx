96db28ed7794c06f9610976226d39ede14ea1e3f
Thiago Rotondo <Rotondo@users.noreply.github.com>
2025-05-21 15:10:36 -0300

    Create OportunidadesPage.tsx

---
+++ src/pages/oportunidades/OportunidadesPage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const OportunidadesPage: React.FC = () => {
+  return (
+    <div>
+      <h1 className="text-2xl font-bold mb-4">Oportunidades</h1>
+      <p>Página de Oportunidades em construção.</p>
+    </div>
+  );
+};
+
+export default OportunidadesPage;
