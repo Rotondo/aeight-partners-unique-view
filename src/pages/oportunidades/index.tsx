afbf3d82f8dd3496c9c64cdb9a44f21cb9241df5
Thiago Rotondo <Rotondo@users.noreply.github.com>
2025-06-25 11:11:23 -0300

    Update index.tsx

--- src/pages/oportunidades/index.tsx
+++ src/pages/oportunidades/index.tsx
@@ -1,13 +1,17 @@
-
 import React from "react";
 import { OportunidadesProvider } from "@/components/oportunidades/OportunidadesContext";
 import { OportunidadesPage } from "@/components/oportunidades/OportunidadesPage";
 import { DemoModeIndicator } from "@/components/privacy/DemoModeIndicator";
+import { DemoModeToggle } from "@/components/privacy/DemoModeToggle";
 
 const OportunidadesPageContainer = () => (
   <OportunidadesProvider>
     <div className="space-y-4">
-      <DemoModeIndicator />
+      {/* Área superior com indicador e botão de alternância do modo demo */}
+      <div className="flex items-center justify-between">
+        <DemoModeIndicator />
+        <DemoModeToggle />
+      </div>
       <OportunidadesPage />
     </div>
   </OportunidadesProvider>
